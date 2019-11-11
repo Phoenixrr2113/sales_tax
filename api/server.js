19f164b76072237d530a1c86d02c06202723375f
Randy Wilson <contact@randywilson.io>
2019-11-11 12:31:44 -0500

    fixed coors error

--- api/server.js
+++ api/server.js
@@ -1,18 +1,31 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require("cors");
 const db = require('../db')
 
 
 
 const server = express()
+server.use(cors());
+server.use(bodyParser.json());
 
 server.get('/', async (req, res) => {
+	res.status(200).json({ api: 'running'})
+})
+
+server.get('/all_states', async (req, res) => {
+
+	let page = req.query.page;
+    let limit = req.query.limit;
+
+
 	res.status(200).json({ api: 'running'})
 })
 
 
 server.get('/:zip', (req, res) => {
 	const zip = parseInt(req.params.zip);
-	res.status(200).json({ [`${zip}`]:
+	res.status(200).send({ [`${zip}`]:
 	db[zip]})
 })
 
